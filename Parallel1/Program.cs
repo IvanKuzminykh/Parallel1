52cbefbc466b4452ad954578549042e2c1153625
IvanKuzminyh <ivan.kuzminyh2019@gmail.com>
2023-03-11 15:42:56 +0500

    Added Parallel class

--- Parallel1/Program.cs
+++ Parallel1/Program.cs
@@ -1,14 +1,16 @@
-﻿using System.Data.Common;
+﻿using Parallel1;
+using System.Data.Common;
 using System.Threading;
 class Program
 {
     static void Main(string[] args)
     {
-
+        Sequential seq = new Sequential();
         double[,] a = { { 1, -2, 5 }, { 3, -1, 0 }, { 2, 1, 3} };
         double[,] b = { { 5, 6 }, { 1, -4 }, { -2, 1 } };
-        double[,] c = MatrixMultiplication(a, b);
-        double[,] invA = InverseMatrix(a);
-        Print(MatrixMultiplication(a, invA));
+        double[,] c = seq.MatrixMultiplication(a, b);
+        double[,] invA = seq.InverseMatrix(a);
+        seq.Print(seq.MatrixMultiplication(a, invA));
+        seq.Print(c);
     }
 }
